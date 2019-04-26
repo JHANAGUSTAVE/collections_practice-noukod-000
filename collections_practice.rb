843e8df23f300afd1723134cd379c50055c2df32
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-26 17:13:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
+def sort_array_asc(array)
+    array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort {|left, right| left.length <=> right.length}
+end
+
+def swap_elements(array)
+  swap_elements_from_to()
+end
+def swap_elements_from_to(array, index, destination_index)
+  array.sort!
+  array.to_s
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+end
